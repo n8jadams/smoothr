931178fd65bb1f5f9b676402628479f147228e0c
n8jadams <nateisneato@gmail.com>
2019-03-07 07:02:50 -0700

    Updated Demo URL, max history size, fixed flashing on animationend

--- src/utils/lsArray.js
+++ src/utils/lsArray.js
@@ -1,12 +1,16 @@
 function getLSArray(key) {
   let val = JSON.parse(localStorage.getItem(key));
-  return !val ? [] : [...val];
+  return !val ? [] : [...val].filter(k => k);
 }
 
 function pushLSArray(key, val, force = false) {
   let lsArray = getLSArray(key);
-  if (force || lsArray.indexOf(val) === -1) {
+  if (typeof val === 'string' && (force || lsArray.indexOf(val) === -1)) {
     lsArray.push(val);
+  }
+  // Arbitrary max amount of keys
+  if (lsArray.length >= 100) {
+    lsArray = [];
   }
   localStorage.setItem(key, JSON.stringify(lsArray));
 }
